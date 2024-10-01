fca7714c9eee5848ac8040baa4b7447a22507a1c
Sahas Subramanian <sahas.subramanian@proton.me>
2024-10-01 15:57:25 +0200

    Validate acyclicity and connectedness of `ComponentGraph`s
    Signed-off-by: Sahas Subramanian <sahas.subramanian@proton.me>

---
+++ src/graph/validation.rs
@@ -0,0 +1,37 @@
+// License: MIT
+// Copyright © 2024 Frequenz Energy-as-a-Service GmbH
+
+//! Methods for validating a [`ComponentGraph`].
+
+mod validate_graph;
+
+use crate::{ComponentGraph, Edge, Error, Node};
+
+pub(crate) struct ComponentGraphValidator<'a, N, E>
+where
+    N: Node,
+    E: Edge,
+{
+    cg: &'a ComponentGraph<N, E>,
+    root: &'a N,
+}
+
+pub(crate) fn validate<N, E>(cg: &ComponentGraph<N, E>) -> Result<(), Error>
+where
+    N: Node,
+    E: Edge,
+{
+    let Ok(root) = cg.component(cg.root_id) else {
+        return Err(Error::internal(format!(
+            "Grid component not found with detected component ID: {}.",
+            cg.root_id
+        )));
+    };
+
+    let validator = ComponentGraphValidator { cg, root };
+
+    validator.validate_acyclicity(root, vec![])?;
+    validator.validate_connected_graph(root)?;
+
+    Ok(())
+}
