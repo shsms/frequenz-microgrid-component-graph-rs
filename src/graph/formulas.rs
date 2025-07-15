cb1386765584fbd84862c4f18778d4d814b55d86
Sahas Subramanian <sahas.subramanian@proton.me>
2025-07-15 16:19:23 +0200

    Improve formula doc-comments
    Signed-off-by: Sahas Subramanian <sahas.subramanian@proton.me>

--- src/graph/formulas.rs
+++ src/graph/formulas.rs
@@ -24,37 +24,40 @@ where
     N: Node,
     E: Edge,
 {
-    /// Returns a string representing the consumer formula for the graph.
+    /// Returns the consumer formula for the graph.
     pub fn consumer_formula(&self) -> Result<Formula, Error> {
         generators::consumer::ConsumerFormulaBuilder::try_new(self)?.build()
     }
 
-    /// Returns a string representing the grid formula for the graph.
+    /// Returns the grid formula for the graph.
     pub fn grid_formula(&self) -> Result<Formula, Error> {
         generators::grid::GridFormulaBuilder::try_new(self)?.build()
     }
 
-    /// Returns a string representing the producer formula for the graph.
+    /// Returns the producer formula for the graph.
     pub fn producer_formula(&self) -> Result<Formula, Error> {
         generators::producer::ProducerFormulaBuilder::try_new(self)?.build()
     }
 
-    /// Returns a string representing the battery formula for the graph.
+    /// Returns the battery formula with the given battery IDs.
+    ///
+    /// If `battery_ids` is `None`, the formula will contain all batteries in
+    /// the graph.
     pub fn battery_formula(&self, battery_ids: Option<BTreeSet<u64>>) -> Result<Formula, Error> {
         generators::battery::BatteryFormulaBuilder::try_new(self, battery_ids)?.build()
     }
 
-    /// Returns a string representing the CHP formula for the graph.
+    /// Returns the CHP formula for the graph.
     pub fn chp_formula(&self, chp_ids: Option<BTreeSet<u64>>) -> Result<Formula, Error> {
         generators::chp::CHPFormulaBuilder::try_new(self, chp_ids)?.build()
     }
 
-    /// Returns a string representing the PV formula for the graph.
+    /// Returns the PV formula for the graph.
     pub fn pv_formula(&self, pv_inverter_ids: Option<BTreeSet<u64>>) -> Result<Formula, Error> {
         generators::pv::PVFormulaBuilder::try_new(self, pv_inverter_ids)?.build()
     }
 
-    /// Returns a string with the coalesce formula for the given component IDs.
+    /// Returns the coalesce formula for the given component IDs.
     ///
     /// This formula uses the `COALESCE` function to return the first non-null
     /// value from the components with the provided IDs.
